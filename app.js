b1b6be3a32c5b21d4cc7d2e7601b4e488acdfeda
Erica <ericaewright@gmail.com>
2018-01-23 11:05:33 -0500

    back buttons, text coloring, etc.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let questions = [
-  "What is the last name of your favorite 90's actress?",
-  "What was your favorite type of candy in the 5th grade?",
-  "Some other question",
-  "And one more for kicks"
+  {text: "What was your favorite candy when you were ten years old?", hint: "e.g. Runts, Twix"},
+  {text: "When you were a teenager, what was the last name of your favourite actress?", hint: "e.g. Barrymore, Bassett"},
+  {text: "What is the last name of your favorite teacher growing up?", hint: "e.g. Johnson, Adler "},
+  {text: "What is the most dependable tool in your toolbox?", hint: "e.g. pliers, kindness"},
+  {text: "What is the last name of your favorite athlete?", hint: "e.g. Sanders, Williams"},
+  {text: "What is your favorite, oddly specific color?", hint: "e.g. fuschia, seafoam "},
+  {text: "What’s a place in the world that you’ve always wanted to go?", hint: "e.g., Hogwarts, Santiago"},
+  {text: "What’s the name of the street you were last on with a friend?", hint: "e.g., Franklin, Main"},
+  {text: "What’s a topic you wish you knew more about?", hint: "e.g., carpentry, dressage"},
+  {text: "Who’s the person that makes you laugh the hardest?", hint: "e.g., Jessica, Seinfeld"},
 ];
 let word_list = [
   'copper',
@@ -136,12 +142,30 @@ let runTest = function() {
   let go_back = document.getElementById('go-back');
   let use_this = document.getElementById('use-this');
   let final_password = '';
-  let password_input = document.getElementById('password');
   let dismiss = document.getElementById('dismiss');
+  let password_input = document.getElementById('password');
+  let name_input = document.getElementById('name');
+  let address_input = document.getElementById('address');
+  let username_input = document.getElementById('username');
+  let notes_input = document.getElementById('notes');
+
+
 
   let toggleDoorHanger = function() {
     doorhanger.setAttribute('open', doorhanger.getAttribute('open') === 'open' ? 'closed' : 'open');
     doorhanger.setAttribute('state', 'entrylist');
+    document.body.setAttribute('open', doorhanger.getAttribute('open'));
+
+    // reset everything incase they begin again
+    password_input.value = '';
+    name_input.value = '';
+    address_input.value = '';
+    username_input.value = '';
+    notes_input.value = '';
+    answer_input.value = '';
+
+    password_input.setAttribute('type', 'password');
+    answer = '';
   }
 
   let createnewEntry = function() {
@@ -149,6 +173,7 @@ let runTest = function() {
       return;
     }
     doorhanger.setAttribute('state', 'new-entry');
+    enableSave();
   }
 
   let showPrompt = function() {
@@ -157,7 +182,7 @@ let runTest = function() {
     }
     doorhanger.setAttribute('state', 'generate');
     shuffleArray(questions);
-    let question = questions[question_index];
+    let question = questions[question_index].text;
     prompt.innerHTML = question;
   }
 
@@ -184,7 +209,7 @@ let runTest = function() {
   let submitAnswer = function() {
     answer = answer_input.value;
     if (doorhanger.getAttribute('open') != 'open'
-    || !doorhanger.getAttribute('state', 'generate')
+    || doorhanger.getAttribute('state') != 'generate'
     || answer == '') {
       return;
     }
@@ -195,15 +220,18 @@ let runTest = function() {
     doorhanger.setAttribute('state', 'results');
     doorhanger.setAttribute('type', 'passphrase');
     result_input.innerHTML = phrase_result;
+    result_input.removeAttribute('edited');
   }
 
   let resultToggle = function(e) {
     if (e.target == password_switch) {
       result_input.innerHTML = password_result;
       doorhanger.setAttribute('type', 'password');
+      result_input.removeAttribute('edited');
     } else {
       result_input.innerHTML = phrase_result;
       doorhanger.setAttribute('type', 'passphrase');
+      result_input.removeAttribute('edited');
     }
   }
 
@@ -219,6 +247,7 @@ let runTest = function() {
     final_password = result_input.innerText;
     doorhanger.setAttribute('state', 'new-entry');
     password_input.value = final_password;
+    enableSave();
   }
 
   answer_input.onkeyup = function() {
@@ -244,11 +273,17 @@ let runTest = function() {
     if (question_index >= questions.length) {
       question_index = 0;
     }
-    prompt.innerHTML = questions[question_index];
+    prompt.innerHTML = questions[question_index].text;
   }
 
   go_back.onclick = function() {
-    doorhanger.setAttribute('state', 'entrylist');
+  if (doorhanger.getAttribute('state') == 'new-entry') {
+      doorhanger.setAttribute('state', 'entrylist');
+    } else if (doorhanger.getAttribute('state') == 'generate') {
+      doorhanger.setAttribute('state', 'new-entry');
+    } else if (doorhanger.getAttribute('state') == 'results') {
+      doorhanger.setAttribute('state', 'generate');
+    }
   }
 
   document.getElementById('eye').onclick = function(e) {
@@ -259,14 +294,29 @@ let runTest = function() {
     }
   }
 
-  document.getElementById('save-entry').onclick = function(){
+  document.getElementById('save-entry').onclick = function(e) {
+    if (e.target.style.opacity == 1) {
+      return;
+    }
     doorhanger.setAttribute('state', 'confirmation');
   }
 
-  // result_input.oninput = function() {
-  //   answer =
-  //   range.insertNode(element);
-  // }
+  result_input.oninput = function() {
+    result_input.setAttribute('edited', 'true');
+  }
+
+  let enableSave = function() {
+    if (name_input.value != ''
+        || address_input.value != ''
+        || username_input.value != ''
+        || password_input.value != '') {
+      document.getElementById('save-entry').style.opacity = '0';
+    } else {
+      document.getElementById('save-entry').style.opacity = '1';
+    }
+  }
+
+  document.querySelector('.new-entry-inputs').addEventListener('keyup', enableSave);
 
   dismiss.onclick = toggleDoorHanger;
   use_this.onclick = newEntryFilled;
